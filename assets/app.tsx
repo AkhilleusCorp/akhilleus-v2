662d217a08ba7d34cac940b1f834043e9cb1cd85
Quentin Périgaud <quentin.perigaud@joingsg.com>
2024-09-03 10:43:35 +0200

    refactoring - remove unused composer dependancies - improve TS config

--- assets/app.tsx
+++ assets/app.tsx
@@ -1,2 +1,20 @@
-import './bootstrap.tsx';
 import './styles/app.css';
+
+enum PersonType {
+    ADMIN = 'admin',
+    USER = 'user'
+}
+
+type Person = {
+    name: string;
+    age: number;
+    role: PersonType
+}
+
+const bob: Person = {
+    name: 'bob',
+    age: 30,
+    role: PersonType.USER
+};
+
+console.log(bob);
