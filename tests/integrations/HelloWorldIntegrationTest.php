fdde4e68abf68f4544e5051f5e4a4db95af9056b
ghriim <vignierquentin@yahoo.fr>
2024-09-21 20:29:42 +0200

    Add default test for integration

---
+++ tests/integrations/HelloWorldIntegrationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\integrations;
+
+use PHPUnit\Framework\TestCase;
+
+final class HelloWorldIntegrationTest extends TestCase
+{
+    public function testHelloWorld(): void
+    {
+        $this->assertTrue(true);
+    }
+}
