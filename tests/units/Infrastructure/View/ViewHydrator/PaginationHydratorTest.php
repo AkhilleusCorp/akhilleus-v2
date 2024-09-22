34b5c71e9f8b0a0299c725812c3130af530e2bd9
Quentin Périgaud <quentin.perigaud@joingsg.com>
2024-09-22 14:04:51 +0200

    Add tests on PaginationHydrator

---
+++ tests/units/Infrastructure/View/ViewHydrator/PaginationHydratorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Tests\units\Infrastructure\View\ViewHydrator;
+
+use App\Infrastructure\View\ViewHydrator\PaginationHydrator;
+use PHPUnit\Framework\TestCase;
+
+final class PaginationHydratorTest extends TestCase
+{
+    /**
+     * @dataProvider countHigherThan0DataProvider
+     */
+    public function testCountHigherThan0(int $count, int $page, int $limit, int $expectedLastPage): void
+    {
+        $hydrator = new PaginationHydrator($count, $page, $limit);
+        $pagination = $hydrator->hydrate()['pagination'];
+
+
+        $this->assertEquals($count, $pagination['count']);
+        $this->assertEquals(PaginationHydrator::DEFAULT_FIRST_PAGE, $pagination['firstPage']);
+        $this->assertEquals($page, $pagination['currentPage']);
+        $this->assertEquals($expectedLastPage, $pagination['lastPage']);
+    }
+
+    public function testCountEqual0()
+    {
+        $hydrator = new PaginationHydrator(0, 1, 100);
+        $pagination = $hydrator->hydrate()['pagination'];
+
+        $this->assertEquals(0, $pagination['count']);
+        $this->assertEquals(PaginationHydrator::DEFAULT_FIRST_PAGE, $pagination['firstPage']);
+        $this->assertEquals(PaginationHydrator::DEFAULT_FIRST_PAGE, $pagination['currentPage']);
+        $this->assertEquals(PaginationHydrator::DEFAULT_FIRST_PAGE, $pagination['lastPage']);
+    }
+
+    public function countHigherThan0DataProvider(): array
+    {
+        return [
+            [10, 1, 10, 1],
+            [39, 3, 10, 4],
+            [123, 2, 25, 5],
+            [1465, 10, 100, 15],
+        ];
+    }
+}
