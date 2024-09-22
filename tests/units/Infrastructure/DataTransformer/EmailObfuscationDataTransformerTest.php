a2bbe03eafaeec23a2da9212a328c190c929deeb
Quentin Périgaud <quentin.perigaud@joingsg.com>
2024-09-22 11:57:37 +0200

    Add test on data transformer

---
+++ tests/units/Infrastructure/DataTransformer/EmailObfuscationDataTransformerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests\units\Infrastructure\DataTransformer;
+
+use App\Infrastructure\DataTransformer\EmailObfuscationDataTransformer;
+use PHPUnit\Framework\TestCase;
+
+final class EmailObfuscationDataTransformerTest extends TestCase
+{
+    /**
+     * @dataProvider validEmailDataProvider
+     */
+    public function testWithValidEmail(string $validEmail, string $expectedResult): void
+    {
+        $result = EmailObfuscationDataTransformer::obfuscate($validEmail);
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * @dataProvider invalidEmailDataProvider
+     */
+    public function testWithInvalidEmail(string $invalidEmail): void
+    {
+        $this->expectException(\LogicException::class);
+
+        EmailObfuscationDataTransformer::obfuscate($invalidEmail);
+    }
+
+    public function validEmailDataProvider(): array
+    {
+        return [
+            ['test@test.com', 't***@t***.com'],
+            ['bob-is-great@fakemail.fr', 'b***********@f*******.fr'],
+            ['garry.host@incognito.io', 'g*********@i********.io'],
+        ];
+    }
+
+    public function invalidEmailDataProvider(): array
+    {
+        return [
+            ['this-is-not-an-email'],
+            ['this-is-not-an-email@mail'],
+            ['this-is-not-an-email.com'],
+            ['@test.com']
+        ];
+    }
+}
