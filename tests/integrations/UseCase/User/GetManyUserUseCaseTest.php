dd044c590018483998818d4b7722a1dd534035e4
Quentin Périgaud <quentin.perigaud@joingsg.com>
2024-09-22 16:56:48 +0200

    Add test case for member data profile

--- tests/integrations/UseCase/User/GetManyUserUseCaseTest.php
+++ tests/integrations/UseCase/User/GetManyUserUseCaseTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\integrations\UseCase\User;
 use App\Domain\DTO\FilterModel\User\GetManyUsersFilterModel;
 use App\Domain\Factory\FilterModelFactory\User\UsersFilterModelModelFactory;
 use App\Domain\Gateway\Provider\User\UserDTOProviderGateway;
+use App\Infrastructure\Exception\InvalidDataProfileException;
 use App\Infrastructure\Registry\DataProfileRegistry;
 use App\Infrastructure\View\ViewModel\PaginationViewModel;
 use App\Infrastructure\View\ViewModel\User\MultipleUserItemViewModel;
@@ -41,5 +42,29 @@ final class GetManyUserUseCaseTest extends KernelTestCase
         $this->assertEquals(GetManyUsersFilterModel::DEFAULT_PAGE, $pagination->firstPage);
         $this->assertEquals(GetManyUsersFilterModel::DEFAULT_PAGE, $pagination->currentPage);
         $this->assertEquals(3, $pagination->lastPage);
+    }
+
+
+
+    public function testGetManyUserWithNoFiltersForMemberDataProfile(): void
+    {
+        $view = $this->useCase->execute([], DataProfileRegistry::DATA_PROFILE_MEMBER);
+
+        /** @var MultipleUserItemViewModel[] $users */
+        $users = $view->data;
+        /** @var PaginationViewModel $pagination */
+        $pagination = $view->extra['pagination'];
+
+        $this->assertCount(GetManyUsersFilterModel::DEFAULT_LIMIT, $users);
+        $this->assertEquals(GetManyUsersFilterModel::DEFAULT_PAGE, $pagination->firstPage);
+        $this->assertEquals(GetManyUsersFilterModel::DEFAULT_PAGE, $pagination->currentPage);
+        $this->assertEquals(3, $pagination->lastPage);
+    }
+
+    public function testGetManyUserWithNoFiltersForUnknownDataProfile(): void
+    {
+        $this->expectException(InvalidDataProfileException::class);
+
+        $this->useCase->execute([], 'unknown_data_profile');
     }
 }
