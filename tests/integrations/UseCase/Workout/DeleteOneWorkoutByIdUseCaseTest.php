9cc63a68e51df8f5af4095871bda55c9b46908c6
Quentin Périgaud <quentin.perigaud@joingsg.com>
2024-09-24 15:03:40 +0200

    Handle workout delete API + tests

---
+++ tests/integrations/UseCase/Workout/DeleteOneWorkoutByIdUseCaseTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Tests\integrations\UseCase\Workout;
+
+use App\Domain\Gateway\Persister\Workout\WorkoutDataModelPersisterGateway;
+use App\Domain\Gateway\Provider\Workout\WorkoutDataModelProviderGateway;
+use App\Infrastructure\Repository\Workout\WorkoutDataModelRepository;
+use App\Tests\integrations\AbstractIntegrationTest;
+use App\UseCase\Workout\DeleteOneWorkoutByIdUseCase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+final class DeleteOneWorkoutByIdUseCaseTest extends AbstractIntegrationTest
+{
+    private DeleteOneWorkoutByIdUseCase $useCase;
+    private WorkoutDataModelRepository $workoutDTORepository;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->useCase = new DeleteOneWorkoutByIdUseCase(
+            $this->container->get(WorkoutDataModelProviderGateway::class),
+            $this->container->get(WorkoutDataModelPersisterGateway::class)
+        );
+
+        $this->workoutDTORepository = $this->container->get(WorkoutDataModelRepository::class);
+    }
+
+    public function testDeleteExistingWorkout(): void
+    {
+        $workoutId = 1;
+
+        $countBeforeDelete = $this->workoutDTORepository->countWorkoutsByParameters(null);
+        $this->useCase->execute($workoutId);
+        $countAfterDelete = $this->workoutDTORepository->countWorkoutsByParameters(null);
+        $workoutPostDelete = $this->workoutDTORepository->getWorkoutById($workoutId);
+
+        $this->assertEquals(($countBeforeDelete - 1), $countAfterDelete);
+        $this->assertNull($workoutPostDelete);
+    }
+
+    public function testDeleteNonExistingWorkout(): void
+    {
+        $this->expectException(NotFoundHttpException::class);
+
+        $this->useCase->execute(666);
+    }
+}
