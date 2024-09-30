5d7f6e5b4b6fc66f3a663307b62e2af390907358
Quentin Périgaud <quentin.*******@******.com>
2024-09-30 21:10:38 +0200

    WIP: Coach registration form

--- src/Infrastructure/Controller/Website/RegistrationController.php
+++ src/Infrastructure/Controller/Website/RegistrationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Infrastructure\Controller\Website;
 
+use App\Domain\Registry\User\UserTypeRegistry;
+use App\Infrastructure\Registry\DataProfileRegistry;
 use App\UseCase\API\User\CreateOneUserUseCase;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,6 +24,24 @@ final class RegistrationController extends AbstractController
         }
 
         return $this->render('website/pages/registration.html.twig');
+    }
+
+    #[Route('/registration/coach', name:'website_registration_coach', methods: ['GET', 'POST'])]
+    public function registrationCoach(Request $request, CreateOneUserUseCase $useCase): Response
+    {
+        if (null !== $this->getUser()) {
+            return $this->redirectToRoute('website_home');
+        }
+
+        if (Request::METHOD_POST === $request->getMethod()) {
+            $useCase->execute(
+                $request->request->all(),
+                DataProfileRegistry::DATA_PROFILE_COACH,
+                UserTypeRegistry::USER_TYPE_COACH
+            );
+        }
+
+        return $this->render('website/pages/registration_coach.html.twig');
     }
 
     #[Route('/registration/success', name:'website_registration_success', methods: ['GET'])]
