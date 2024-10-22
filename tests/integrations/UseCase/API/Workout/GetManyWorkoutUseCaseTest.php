d1c3dd598d9787ef36552addebe001c1a9233f56
Quentin Périgaud <quentin.*******@******.com>
2024-10-22 15:13:59 +0200

    Work on using token payload to build dynamic filters

--- tests/integrations/UseCase/API/Workout/GetManyWorkoutUseCaseTest.php
+++ tests/integrations/UseCase/API/Workout/GetManyWorkoutUseCaseTest.php
@@ -2,10 +2,12 @@
 
 namespace App\Tests\integrations\UseCase\API\Workout;
 
-use App\Domain\DTO\FilterModel\Workout\GetManyWorkoutsFilterModel;
+use App\Domain\DTO\FilterModel\AbstractFilterModel;
 use App\Domain\Factory\FilterModelFactory\Workout\WorkoutsFilterModelModelFactory;
 use App\Domain\Gateway\Provider\Workout\WorkoutDataModelProviderGateway;
+use App\Domain\Registry\User\UserTypeRegistry;
 use App\Domain\Registry\Workout\WorkoutStatusRegistry;
+use App\Infrastructure\DTO\TokenPayloadDTO;
 use App\Infrastructure\Registry\DataProfileRegistry;
 use App\Infrastructure\View\ViewModel\PaginationViewModel;
 use App\Infrastructure\View\ViewModel\Workout\MultipleWorkoutItemDataViewModel;
@@ -30,43 +32,67 @@ final class GetManyWorkoutUseCaseTest extends AbstractIntegrationTest
 
     public function testGetManyWorkoutWithNoFiltersForAdminDataProfile(): void
     {
-        $view = $this->useCase->execute([], DataProfileRegistry::DATA_PROFILE_ADMIN);
+        $payload = new TokenPayloadDTO();
+        $payload->userId = 3;
+        $payload->userType = UserTypeRegistry::USER_TYPE_ADMIN;
+
+        $view = $this->useCase->execute(
+            [],
+            $payload,
+            DataProfileRegistry::DATA_PROFILE_ADMIN
+        );
 
         /** @var MultipleWorkoutItemDataViewModel[] $workouts */
         $workouts = $view->data;
         /** @var PaginationViewModel $pagination */
         $pagination = $view->extra['pagination'];
 
-        $this->assertCount(GetManyWorkoutsFilterModel::DEFAULT_LIMIT, $workouts);
-        $this->assertEquals(GetManyWorkoutsFilterModel::DEFAULT_PAGE, $pagination->firstPage);
-        $this->assertEquals(GetManyWorkoutsFilterModel::DEFAULT_PAGE, $pagination->currentPage);
+        $this->assertCount(AbstractFilterModel::DEFAULT_LIMIT, $workouts);
+        $this->assertEquals(AbstractFilterModel::DEFAULT_PAGE, $pagination->firstPage);
+        $this->assertEquals(AbstractFilterModel::DEFAULT_PAGE, $pagination->currentPage);
         $this->assertEquals(3, $pagination->lastPage);
     }
 
     public function testGetManyWorkoutWithNoFiltersForMemberDataProfile(): void
     {
-        $view = $this->useCase->execute([]);
+        $payload = new TokenPayloadDTO();
+        $payload->userId = 1;
+        $payload->userType = UserTypeRegistry::USER_TYPE_MEMBER;
+
+        $view = $this->useCase->execute([], $payload);
 
         /** @var MultipleWorkoutItemDataViewModel[] $workouts */
         $workouts = $view->data;
         /** @var PaginationViewModel $pagination */
         $pagination = $view->extra['pagination'];
 
-        $this->assertCount(GetManyWorkoutsFilterModel::DEFAULT_LIMIT, $workouts);
-        $this->assertEquals(GetManyWorkoutsFilterModel::DEFAULT_PAGE, $pagination->firstPage);
-        $this->assertEquals(GetManyWorkoutsFilterModel::DEFAULT_PAGE, $pagination->currentPage);
+        $this->assertCount(AbstractFilterModel::DEFAULT_LIMIT, $workouts);
+        $this->assertEquals(AbstractFilterModel::DEFAULT_PAGE, $pagination->firstPage);
+        $this->assertEquals(AbstractFilterModel::DEFAULT_PAGE, $pagination->currentPage);
         $this->assertEquals(3, $pagination->lastPage);
     }
 
     public function testGetManyWorkoutWithFilterStatusesFilter(): void
     {
-        $view = $this->useCase->execute(['status' => WorkoutStatusRegistry::WORKOUT_STATUS_IN_PROGRESS], DataProfileRegistry::DATA_PROFILE_ADMIN);
+        $payload = new TokenPayloadDTO();
+        $payload->userId = 3;
+        $payload->userType = UserTypeRegistry::USER_TYPE_ADMIN;
+
+        $view = $this->useCase->execute(
+            ['status' => WorkoutStatusRegistry::WORKOUT_STATUS_IN_PROGRESS],
+            $payload,
+            DataProfileRegistry::DATA_PROFILE_ADMIN
+        );
         $this->assertCount(1, $view->data);
 
-        $view = $this->useCase->execute(['status' => WorkoutStatusRegistry::WORKOUT_STATUS_COMPLETED]);
+        $payload = new TokenPayloadDTO();
+        $payload->userId = 1;
+        $payload->userType = UserTypeRegistry::USER_TYPE_MEMBER;
+
+        $view = $this->useCase->execute(['status' => WorkoutStatusRegistry::WORKOUT_STATUS_COMPLETED], $payload);
         $this->assertCount(25, $view->data);
 
-        $view = $this->useCase->execute(['status' => WorkoutStatusRegistry::WORKOUT_STATUS_PLANNED]);
+        $view = $this->useCase->execute(['status' => WorkoutStatusRegistry::WORKOUT_STATUS_PLANNED], $payload);
         $this->assertCount(1, $view->data);
     }
 }
