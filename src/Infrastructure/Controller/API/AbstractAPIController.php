af73d538a07471bbb213843ccd4fb857da6db090
Quentin Périgaud <quentin.*******@******.com>
2024-10-22 21:13:44 +0200

    Auto add token based on env var for dev purpose

--- src/Infrastructure/Controller/API/AbstractAPIController.php
+++ src/Infrastructure/Controller/API/AbstractAPIController.php
@@ -2,28 +2,37 @@
 
 namespace App\Infrastructure\Controller\API;
 
-use App\Domain\Registry\User\UserTypeRegistry;
 use App\Infrastructure\DTO\TokenPayloadDTO;
+use App\Infrastructure\Registry\TokenPayloadRegistry;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 abstract class AbstractAPIController extends AbstractController
 {
+    public function __construct(private readonly JWTTokenManagerInterface $tokenManager)
+    {
+    }
+
     protected function getTokenPayload(Request $request): ?TokenPayloadDTO
     {
-        if (false === $request->headers->hasCacheControlDirective('Authorization')) {
+        if (false === $request->headers->has('Authorization')) {
             return null;
         }
 
-        $request->headers->get('Authorization');
+        $encodedToken = str_replace('Bearer ', '', $request->headers->get('Authorization'));
 
-        // Todo: extract from token in the future
+        try {
+            $decodedToken = $this->tokenManager->parse($encodedToken);
+        } catch (JWTDecodeFailureException $exception) {
+            throw new AccessDeniedHttpException($exception->getMessage());
+        }
+
         $payload = new TokenPayloadDTO();
-        $payload->userId = 1;
-        $payload->userType = UserTypeRegistry::USER_TYPE_MEMBER;
-
-        // TokenPayloadRegistry::PAYLOAD_USER_ID,
-        // TokenPayloadRegistry::PAYLOAD_USER_TYPE,
+        $payload->userId = $decodedToken[TokenPayloadRegistry::PAYLOAD_USER_ID];
+        $payload->userType = $decodedToken[TokenPayloadRegistry::PAYLOAD_USER_TYPE];
 
         return $payload;
     }
