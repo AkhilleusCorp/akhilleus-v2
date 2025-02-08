957ab71684d993f24740fa593b759136035ad853
Quentin Périgaud <quentin.*******@******.com>
2025-02-08 20:04:05 +0100

    Add missing reference to member on workout fixtures

--- src/Infrastructure/DataFixtures/Workout/WorkoutFixtures.php
+++ src/Infrastructure/DataFixtures/Workout/WorkoutFixtures.php
@@ -56,18 +56,21 @@ final class WorkoutFixtures extends AbstractFixtures
                 'endDate' => null,
                 'plannedDate' => null,
                 'visibility' => WorkoutVisibilityRegistry::WORKOUT_VISIBILITY_PRIVATE,
+                'memberRef' => 'user-ghriim',
             ], [
                 'name' => 'Plan Specific Client',
                 'startDate' => null,
                 'endDate' => null,
                 'plannedDate' => new \DateTimeImmutable('2days'),
                 'visibility' => WorkoutVisibilityRegistry::WORKOUT_VISIBILITY_SPECIFIC_CLIENT,
+                'memberRef' => 'user-ghriim',
             ], [
                 'name' => 'Completed Friends',
                 'startDate' => new \DateTimeImmutable('2hours ago'),
                 'endDate' => new \DateTimeImmutable('1hour ago'),
                 'plannedDate' => null,
                 'visibility' => WorkoutVisibilityRegistry::WORKOUT_VISIBILITY_FRIENDS,
+                'memberRef' => 'user-ghriim',
             ],
         ];
     }
