e65425db3acb70597d89ac60d1cb6e095ea38a07
Quentin Périgaud <quentin.*******@******.com>
2025-02-08 20:08:41 +0100

    Add new movement properties to source model

--- src/Domain/DTO/SourceModel/Workout/UpdateMovementSourceModel.php
+++ src/Domain/DTO/SourceModel/Workout/UpdateMovementSourceModel.php
@@ -7,14 +7,16 @@ use App\Domain\DTO\SourceModel\UpdateSourceModelInterface;
 final class UpdateMovementSourceModel implements UpdateSourceModelInterface
 {
     public string $name;
-
     public string $status;
 
-    public int $primaryMuscle;
+    public bool $hasReps = false;
+    public bool $hasWeight = false;
+    public bool $hasDuration = false;
+    public bool $hasDistance = false;
 
+    public int $primaryMuscle;
     /** @var int[]|null */
     public ?array $auxiliaryMuscles;
-
     /** @var int[]|null */
     public ?array $equipments;
 }
